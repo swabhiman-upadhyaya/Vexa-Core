247b28be88d3fbce8f45040f013ebe2d5b2ef91b
Swabhiman Upadhyaya <swabhimanupadhyaya18@gmail.com>
2026-05-19 08:53:10 +0530

    Integrate Socket.IO for real-time communication

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,15 @@
 import "dotenv/config";
-
 import app from "./src/app.js";
 import connectDB from './src/config/database.js';
 import { testAi } from "./src/services/ai.services.js";
+import http from "http";
+import { initSocket } from './src/sockets/server.socket.js';
 
 const PORT = process.env.PORT || 3000;
+
+const httpServer = http.createServer(app);
+
+initSocket(httpServer);
 
 testAi();
 
@@ -14,6 +19,6 @@ connectDB()
     process.exit(1);
   });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- Frontend/src/features/chat/pages/Dashboard.jsx
+++ Frontend/src/features/chat/pages/Dashboard.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { useChat } from '../hooks/useChat';
+
 
 const Dashboard = () => {
 
+  const chat = useChat();
+
   const user = useSelector((state) => state.auth.user);
+
+  console.log(user)
+
+  useEffect(() => {
+    chat();
+  }, [])
 
   return (
     <div>Dashboard</div>
